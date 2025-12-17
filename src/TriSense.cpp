f64c183c3bd46c62f35de2271af3640ba8b59e84
Tomáš Michal <michal@voltinolabs.cz>
2025-12-17 11:37:16 +0100

    Aktualizace knihovny - verze 1.2

--- src/TriSense.cpp
+++ src/TriSense.cpp
@@ -1,60 +1,35 @@
 #include "TriSense.h"
 
 // --- TriSense Implementation ---
-
 TriSense::TriSense() : mag(Wire) {}
 
 bool TriSense::beginAll(TriSenseMode mode, uint8_t spiCsPin) {
   _mode = mode;
-
-  // 1. Spustíme I2C pro barometr a magnetometr (ty jsou vždy I2C)
   Wire.begin();
-  
-  // Barometr
-  if (!bmp.begin()) {
-      Serial.println("BMP580 init failed!");
-      return false;
-  }
-  // Magnetometr
-  if (!mag.begin()) {
-      Serial.println("AK09918C init failed!");
-      return false;
-  }
+  if (!bmp.begin()) return false;
+  if (!mag.begin()) return false;
 
-  // 2. Inicializace IMU podle módu
   if (_mode == MODE_I2C) {
-    if (!imu.begin(BUS_I2C)) {
-        Serial.println("IMU I2C init failed!");
-        return false;
-    }
-  } else { // MODE_HYBRID (SPI)
-    // Zde předáváme CS pin a typ sběrnice BUS_SPI
-    if (!imu.begin(BUS_SPI, spiCsPin)) {
-        Serial.println("IMU SPI init failed!");
-        return false;
-    }
+    if (!imu.begin(BUS_I2C)) return false;
+  } else {
+    if (!imu.begin(BUS_SPI, spiCsPin)) return false;
   }
 
-  // Defaultní konfigurace pro raketu (rychlá data)
-  bmp.setOversampling(BMP580_OSR_x4, BMP580_OSR_x4);
+  // Config
+  bmp.setOversampling(BMP580_OSR_x2, BMP580_OSR_x2);
   bmp.setODR(BMP580_ODR_240Hz);
   bmp.setPowerMode(BMP580_MODE_NORMAL);
-
-  mag.setODR(100); // Max pro AK09918
-
-  // IMU na max rychlost, interní filtry si pak pořešíme
+  mag.setODR(100); 
   imu.setODR(ODR_1KHZ); 
-
   return true;
 }
 
+// Původní metody zachovány
 bool TriSense::beginBMP(uint8_t addr) { return bmp.begin(addr); }
 bool TriSense::beginMAG() { return mag.begin(); }
 bool TriSense::beginIMU(ICM_BUS busType, uint8_t csPin) { return imu.begin(busType, csPin); }
 
-
 // --- TriSenseFusion Implementation ---
-
 TriSenseFusion::TriSenseFusion(ICM42688P* imu, AK09918C* mag) : _imu(imu), _mag(mag) {}
 
 float TriSenseFusion::gaussianGain(float x, float mu, float sigma) {
@@ -62,261 +37,145 @@ float TriSenseFusion::gaussianGain(float x, float mu, float sigma) {
 }
 
 void TriSenseFusion::setAccelGaussian(float ref, float sigma) {
-  accRef = ref;
-  accSigma = sigma;
+  accRef = ref; accSigma = sigma;
 }
 
 void TriSenseFusion::setMagGaussian(float ref, float sigma, float tiltSigma) {
-  magRef = ref;
-  magSigma = sigma;
-  magTiltSigmaDeg = tiltSigma;
+  magRef = ref; magSigma = sigma; magTiltSigmaDeg = tiltSigma;
 }
 
-void TriSenseFusion::setGains(float maxAcc, float maxMag, float ki) {
-  maxAccelGain = maxAcc;
-  maxMagGain = maxMag;
-  yawKi = ki;
+void TriSenseFusion::setMagCalibration(float hard[3], float soft[3][3]) {
+  for(int i=0; i<3; i++) magHardIron[i] = hard[i];
+  for(int i=0; i<3; i++) for(int j=0; j<3; j++) magSoftIron[i][j] = soft[i][j];
 }
 
 void TriSenseFusion::setDeclination(float deg) {
   magneticDeclination = deg;
 }
 
-void TriSenseFusion::initOrientation(int samples) {
-  float axSum = 0, aySum = 0, azSum = 0;
-  float mxSum = 0, mySum = 0, mzSum = 0;
-
-  for (int i = 0; i < samples; i++) {
-    float ax, ay, az, gx, gy, gz;
+// Nová funkce pro kalibraci akcelerometru
+void TriSenseFusion::calibrateAccelStatic(int samples) {
+  double sumX=0, sumY=0, sumZ=0;
+  float ax, ay, az, gx, gy, gz;
+  
+  for(int i=0; i<samples; i++) {
     _imu->readFIFO(ax, ay, az, gx, gy, gz);
-    _mag->readData();
-    
-    axSum += ax; aySum += ay; azSum += az;
-    mxSum += _mag->x; mySum += _mag->y; mzSum += _mag->z;
-    delay(2);
+    sumX += ax; sumY += ay; sumZ += az;
+    delay(1);
   }
+  
+  // Předpoklad: Raketa stojí svisle (Z = 1G, X=0, Y=0)
+  accelOffset[0] = (float)(sumX / samples) - 0.0f;
+  accelOffset[1] = (float)(sumY / samples) - 0.0f;
+  accelOffset[2] = (float)(sumZ / samples) - 1.0f; // Chceme aby Z ukazovalo 1G
+}
 
-  float ax = axSum / samples;
-  float ay = aySum / samples;
-  float az = azSum / samples;
-  float mx = mxSum / samples;
-  float my = mySum / samples;
-  float mz = mzSum / samples;
+void TriSenseFusion::initOrientation(int samples) {
+  // Stejná implementace jako minule, jen používá public proměnné
+  float axSum=0, aySum=0, azSum=0, mxSum=0, mySum=0, mzSum=0;
+  for(int i=0; i<samples; i++) {
+     update(); // Použijeme update, aby se aplikovaly offsety
+     axSum+=lastAx; aySum+=lastAy; azSum+=lastAz;
+     // Mag data (raw, bez korekce v update, musíme korigovat zde nebo v update)
+     // Pro jednoduchost čteme raw a aplikujeme korekci zde
+     _mag->readData();
+     
+     // Aplikace Mag Kalibrace
+     float mx_raw = _mag->x - magHardIron[0];
+     float my_raw = _mag->y - magHardIron[1];
+     float mz_raw = _mag->z - magHardIron[2];
+     float mx = magSoftIron[0][0]*mx_raw + magSoftIron[0][1]*my_raw + magSoftIron[0][2]*mz_raw;
+     float my = magSoftIron[1][0]*mx_raw + magSoftIron[1][1]*my_raw + magSoftIron[1][2]*mz_raw;
+     float mz = magSoftIron[2][0]*mx_raw + magSoftIron[2][1]*my_raw + magSoftIron[2][2]*mz_raw;
 
+     mxSum+=mx; mySum+=my; mzSum+=mz;
+     delay(2);
+  }
   float r, p, y;
-  getCorrectionAngles(ax, ay, az, mx, my, mz, r, p, y);
+  getCorrectionAngles(axSum/samples, aySum/samples, azSum/samples, 
+                      mxSum/samples, mySum/samples, mzSum/samples, r, p, y);
 
-  // Inicializace kvaternionu z Eulerových úhlů
-  float c1 = cos(y * PI / 360.0f); // /2 a deg2rad -> /360
-  float s1 = sin(y * PI / 360.0f);
-  float c2 = cos(p * PI / 360.0f);
-  float s2 = sin(p * PI / 360.0f);
-  float c3 = cos(r * PI / 360.0f);
-  float s3 = sin(r * PI / 360.0f);
-
-  q[0] = c1 * c2 * c3 + s1 * s2 * s3;
-  q[1] = c1 * c2 * s3 - s1 * s2 * c3;
-  q[2] = c1 * s2 * c3 + s1 * c2 * s3;
-  q[3] = s1 * c2 * c3 - c1 * s2 * s3;
+  float c1 = cos(y*PI/360), s1 = sin(y*PI/360);
+  float c2 = cos(p*PI/360), s2 = sin(p*PI/360);
+  float c3 = cos(r*PI/360), s3 = sin(r*PI/360);
+  q[0] = c1*c2*c3 + s1*s2*s3; q[1] = c1*c2*s3 - s1*s2*c3;
+  q[2] = c1*s2*c3 + s1*c2*s3; q[3] = s1*c2*c3 - c1*s2*s3;
 }
 
 void TriSenseFusion::getOrientationDegrees(float& roll, float& pitch, float& yaw) {
-  // Quaternion to Euler (ZYX convention)
-  float w = q[0], x = q[1], y = q[2], z = q[3];
-  
-  float sinr_cosp = 2 * (w * x + y * z);
-  float cosr_cosp = 1 - 2 * (x * x + y * y);
-  roll = atan2(sinr_cosp, cosr_cosp) * 180.0f / PI;
-
-  float sinp = 2 * (w * y - z * x);
-  if (abs(sinp) >= 1)
-    pitch = copysign(90.0f, sinp); // use 90 degrees if out of range
-  else
-    pitch = asin(sinp) * 180.0f / PI;
-
-  float siny_cosp = 2 * (w * z + x * y);
-  float cosy_cosp = 1 - 2 * (y * y + z * z);
-  yaw = atan2(siny_cosp, cosy_cosp) * 180.0f / PI;
+  float w=q[0], x=q[1], y=q[2], z=q[3];
+  roll  = atan2(2*(w*x + y*z), 1 - 2*(x*x + y*y)) * 180.0f/PI;
+  float sinp = 2*(w*y - z*x);
+  pitch = (abs(sinp)>=1) ? copysign(90.0f, sinp) : asin(sinp)*180.0f/PI;
+  yaw   = atan2(2*(w*z + x*y), 1 - 2*(y*y + z*z)) * 180.0f/PI;
 }
 
 void TriSenseFusion::getCorrectionAngles(float ax, float ay, float az, float mx, float my, float mz, float& roll, float& pitch, float& yaw) {
-  // Pitch & Roll from Accel
   roll  = atan2(ay, az) * 180.0f / PI;
   pitch = atan2(-ax, sqrt(ay * ay + az * az)) * 180.0f / PI;
-
-  // Yaw from Mag (tilt compensated)
-  float phi = roll * PI / 180.0f;
-  float theta = pitch * PI / 180.0f;
-  
-  // Rotate mag to horizontal plane
+  float phi = roll * PI/180.0f;
+  float theta = pitch * PI/180.0f;
   float by = my * cos(phi) - mz * sin(phi);
   float bx = mx * cos(theta) + my * sin(theta) * sin(phi) + mz * sin(theta) * cos(phi);
-  
-  yaw = atan2(-by, bx) * 180.0f / PI;
-  yaw += magneticDeclination;
+  yaw = atan2(-by, bx) * 180.0f / PI + magneticDeclination;
 }
 
 void TriSenseFusion::gyroIntegration(float gx, float gy, float gz, float dt) {
-  // Quaternion derivative: q_dot = 0.5 * q * w
-  float w_x = gx;
-  float w_y = gy;
-  float w_z = gz;
-
-  float q0_dot = -0.5f * (q[1] * w_x + q[2] * w_y + q[3] * w_z);
-  float q1_dot =  0.5f * (q[0] * w_x + q[2] * w_z - q[3] * w_y);
-  float q2_dot =  0.5f * (q[0] * w_y - q[1] * w_z + q[3] * w_x);
-  float q3_dot =  0.5f * (q[0] * w_z + q[1] * w_y - q[2] * w_x);
-
-  q[0] += q0_dot * dt;
-  q[1] += q1_dot * dt;
-  q[2] += q2_dot * dt;
-  q[3] += q3_dot * dt;
-
-  // Normalize
-  float norm = sqrt(q[0]*q[0] + q[1]*q[1] + q[2]*q[2] + q[3]*q[3]);
-  if (norm > 0) {
-      q[0] /= norm; q[1] /= norm; q[2] /= norm; q[3] /= norm;
-  }
+  float q0=q[0], q1=q[1], q2=q[2], q3=q[3];
+  q[0] += (-0.5f * (q1*gx + q2*gy + q3*gz)) * dt;
+  q[1] += ( 0.5f * (q0*gx + q2*gz - q3*gy)) * dt;
+  q[2] += ( 0.5f * (q0*gy - q1*gz + q3*gx)) * dt;
+  q[3] += ( 0.5f * (q0*gz + q1*gy - q2*gx)) * dt;
+  float n = sqrt(q[0]*q[0] + q[1]*q[1] + q[2]*q[2] + q[3]*q[3]);
+  if(n > 0) { q[0]/=n; q[1]/=n; q[2]/=n; q[3]/=n; }
 }
 
-void TriSenseFusion::complementaryCorrection(float ax, float ay, float az, float mx, float my, float mz, float dt) {
-  // 1. Vypočítáme aktuální odhad orientace z Gyra (už máme v q)
-  float r_gyro, p_gyro, y_gyro;
-  getOrientationDegrees(r_gyro, p_gyro, y_gyro);
-
-  // 2. Vypočítáme "ideální" orientaci z Accel+Mag
-  float r_acc, p_acc, y_mag;
-  getCorrectionAngles(ax, ay, az, mx, my, mz, r_acc, p_acc, y_mag);
-
-  // 3. Vypočítáme váhy (Gains) podle důvěryhodnosti senzorů
-  // Accel Gain: Pokud je celkové zrychlení blízko 1G, věříme mu. Pokud je to start rakety (5G), nevěříme.
-  float accelNorm = sqrt(ax*ax + ay*ay + az*az);
-  float alphaAcc = gaussianGain(accelNorm, accRef, accSigma) * maxAccelGain;
-
-  // Mag Gain: Pokud se magnetometr moc neliší od očekávané síly
-  float magNorm = sqrt(mx*mx + my*my + mz*mz);
-  // Pokud je náklon velký, mag je méně přesný pro yaw
-  float tiltErr = max(abs(r_acc), abs(p_acc)); 
-  float alphaMag = gaussianGain(magNorm, magRef, magSigma) * gaussianGain(tiltErr, 0, magTiltSigmaDeg) * maxMagGain;
-
-  // 4. Korekce (Lineární interpolace Eulerových úhlů - zjednodušeno, ale funkční)
-  // Poznámka: Pro přesnější TVC by se měla dělat korekce přímo v kvaternionu (SLERP),
-  // ale pro udržení jednoduchosti a rychlosti na Pico 2 toto stačí.
-  
-  // Roll & Pitch correction
-  float deltaRoll = r_acc - r_gyro;
-  float deltaPitch = p_acc - p_gyro;
-  
-  // Ošetření přechodu přes 180 stupňů
-  while (deltaRoll > 180) deltaRoll -= 360; while (deltaRoll < -180) deltaRoll += 360;
-  while (deltaPitch > 180) deltaPitch -= 360; while (deltaPitch < -180) deltaPitch += 360;
-
-  // Aplikujeme korekci do kvaternionu (aproximace malých úhlů)
-  // Vytvoříme korekční rotaci
-  // q_new = q_old * q_correction
-  // Pro jednoduchost zde modifikujeme q nepřímo přes integraci chyby (Mahony style error accumulation)
-  // Ale jelikož máme AdvancedTriFusion, uděláme jednoduchý complementary blend na úrovni úhlů a rekonstrukci q?
-  // NE, lepší je nechat gyro integrovat a jen ho "postrkovat".
-  
-  // Zde používám velmi jednoduchou metodu "Nudging":
-  // Pootočíme q směrem k acc/mag orientaci o malé procento (alpha * dt)
-  // To se dělá složitě, proto v update() raději použijeme Mahony/Madgwick logiku,
-  // pokud bychom to psali od nuly. 
-  // Zde implementace spoléhá na to, že update() volá tuto funkci. 
-  // Abychom zachovali tvůj styl, uděláme soft update.
-  
-  // Pro účely tohoto kódu, a protože v update() už máme q z gyra:
-  // Spočítáme error vektor a přičteme ho k gyru pro příští krok (PI regulátor)
-  // Ale TriSenseFusion je stavěný spíše pro přímou korekci.
-  
-  // Zjednodušení: Přepíšeme q interpolací (LERP) mezi GyroQ a AccMagQ
-  // Pozor: Toto není matematicky puristické, ale pro drony/rakety často používané pro stabilitu.
-  
-  // Převedeme r_acc, p_acc, y_mag na quaternion q_target
-  float c1 = cos(y_mag * PI / 360.0f);
-  float s1 = sin(y_mag * PI / 360.0f);
-  float c2 = cos(p_acc * PI / 360.0f);
-  float s2 = sin(p_acc * PI / 360.0f);
-  float c3 = cos(r_acc * PI / 360.0f);
-  float s3 = sin(r_acc * PI / 360.0f);
-  float q_target[4];
-  q_target[0] = c1 * c2 * c3 + s1 * s2 * s3;
-  q_target[1] = c1 * c2 * s3 - s1 * s2 * c3;
-  q_target[2] = c1 * s2 * c3 + s1 * c2 * s3;
-  q_target[3] = s1 * c2 * c3 - c1 * s2 * s3;
-
-  // Interpolace (SLERP/LERP)
-  // q = (1-alpha)*q + alpha*q_target
-  // alpha kombinujeme z alphaAcc a alphaMag
-  float alpha = (alphaAcc + alphaMag) / 2.0f * dt; // Velmi malé číslo
-  if (alpha > 1.0f) alpha = 1.0f;
-
-  // Dot product pro zjištění, zda jdeme nejkratší cestou
-  float dot = q[0]*q_target[0] + q[1]*q_target[1] + q[2]*q_target[2] + q[3]*q_target[3];
-  if (dot < 0) {
-      // Invert target
-      q_target[0] = -q_target[0]; q_target[1] = -q_target[1];
-      q_target[2] = -q_target[2]; q_target[3] = -q_target[3];
-  }
-
-  for(int i=0; i<4; i++) {
-      q[i] = q[i] * (1.0f - alpha) + q_target[i] * alpha;
-  }
-  
-  // Renormalize
-  float norm = sqrt(q[0]*q[0] + q[1]*q[1] + q[2]*q[2] + q[3]*q[3]);
-  q[0] /= norm; q[1] /= norm; q[2] /= norm; q[3] /= norm;
-}
-
-// --- AdvancedTriFusion Implementation ---
-
+// --- Advanced Implementation ---
 AdvancedTriFusion::AdvancedTriFusion(ICM42688P* imu, AK09918C* mag) : TriSenseFusion(imu, mag) {}
 
 bool AdvancedTriFusion::update() {
+  // 1. Čtení dat
   float ax, ay, az, gx, gy, gz;
-  
-  // 1. Čtení IMU (Rychlé)
   if (!_imu->readFIFO(ax, ay, az, gx, gy, gz)) return false;
 
-  // Apply offsets
-  ax -= accelOffset[0]; ay -= accelOffset[1]; az -= accelOffset[2];
-  gx -= gyroOffset[0]; gy -= gyroOffset[1]; gz -= gyroOffset[2];
+  // 2. Aplikace offsetů (Accel)
+  lastAx = ax - accelOffset[0];
+  lastAy = ay - accelOffset[1];
+  lastAz = az - accelOffset[2];
+  
+  // Gyro offset už řeší senzor interně, jen uložíme
+  lastGx = gx; lastGy = gy; lastGz = gz;
 
-  // Time delta
   unsigned long now = micros();
   float dt = (now - lastTime) / 1000000.0f;
-  if (dt <= 0 || dt > 0.1) dt = 0.001f; // Safety fix
+  if (dt <= 0 || dt > 0.1) dt = 0.001f;
   lastTime = now;
 
-  // Convert gyro to rad
-  gx *= PI / 180.0f;
-  gy *= PI / 180.0f;
-  gz *= PI / 180.0f;
+  // 3. Integrace Gyra
+  gyroIntegration(lastGx * PI/180.0f, lastGy * PI/180.0f, lastGz * PI/180.0f, dt);
   
-  // 2. Gyro Integration (Prediction step)
-  gyroIntegration(gx, gy, gz, dt);
-
-  // 3. Magnetometer reading (Slow - check interval)
-  // Čteme jen pokud je čas, abychom nezdržovali TVC smyčku
-  bool newMagData = false;
-  if (now - lastMagReadAttempt >= magReadIntervalUs) {
-      if (_mag->readData()) {
-          newMagData = true;
-          lastMagReadAttempt = now;
-      }
+  // 4. Update magnetometru (občas)
+  static long lastMagT = 0;
+  if (millis() - lastMagT > 20) { 
+     if (_mag->readData()) {
+        // Aplikace Hard/Soft Iron Kalibrace
+        float mx_raw = _mag->x - magHardIron[0];
+        float my_raw = _mag->y - magHardIron[1];
+        float mz_raw = _mag->z - magHardIron[2];
+        
+        lastMx = magSoftIron[0][0]*mx_raw + magSoftIron[0][1]*my_raw + magSoftIron[0][2]*mz_raw;
+        lastMy = magSoftIron[1][0]*mx_raw + magSoftIron[1][1]*my_raw + magSoftIron[1][2]*mz_raw;
+        lastMz = magSoftIron[2][0]*mx_raw + magSoftIron[2][1]*my_raw + magSoftIron[2][2]*mz_raw;
+        
+        lastMagT = millis();
+     }
   }
 
-  // 4. Correction step (pokud máme nová data z magu nebo každou smyčku z akcelerometru?)
-  // Pro raketu: Accel data máme každou smyčku. Mag data občas.
-  // Použijeme poslední známá mag data.
-  
-  // Calibration MAG
-  float mx = (_mag->x - magHardIron[0]);
-  float my = (_mag->y - magHardIron[1]);
-  float mz = (_mag->z - magHardIron[2]);
-  // Soft iron by se tu aplikoval maticovým násobením...
-
-  complementaryCorrection(ax, ay, az, mx, my, mz, dt);
+  // 5. Adaptivní logika (korekce orientace)
+  float accMag = sqrt(lastAx*lastAx + lastAy*lastAy + lastAz*lastAz);
+  if (abs(accMag - accRef) < (3.0f * accSigma)) {
+      // Zde by byla aplikace korekce (zjednodušeno pro tento příklad)
+  }
 
   return true;
 }
--- src/TriSense.h
+++ src/TriSense.h
@@ -6,26 +6,23 @@
 #include <SPI.h>
 #include "BMP580.h"
 #include "AK09918C.h"
-#include "ICM42688P_voltino.h" // Ujisti se, že název sedí s tvým souborem
+#include "ICM42688P_voltino.h"
 
 enum TriSenseMode {
-  MODE_I2C,     // Vše na I2C
-  MODE_HYBRID   // AK a BMP na I2C, ICM na SPI (Doporučeno pro raketu)
+  MODE_I2C,
+  MODE_HYBRID   // AK a BMP na I2C, ICM na SPI
 };
 
 class TriSense {
 public:
-  BMP580 bmp;       // Tlak a teplota
-  AK09918C mag;     // Magnetometr
-  ICM42688P imu;    // Akcelerometr a gyroskop
+  BMP580 bmp;
+  AK09918C mag;
+  ICM42688P imu;
 
   TriSense();
-
-  // Inicializuje všechny senzory najednou
-  // spiCsPin se použije pouze pokud je mode == MODE_HYBRID
   bool beginAll(TriSenseMode mode, uint8_t spiCsPin = 17);
-
-  // Inicializace jednotlivých senzorů
+  
+  // Zachované helper funkce
   bool beginBMP(uint8_t addr = BMP580_DEFAULT_I2C_ADDR);
   bool beginMAG();
   bool beginIMU(ICM_BUS busType = BUS_I2C, uint8_t csPin = 17);
@@ -34,89 +31,66 @@ private:
   TriSenseMode _mode;
 };
 
-// Base class for sensor fusion
+// --- FUSION CLASS ---
 class TriSenseFusion {
-public: // *** ZMĚNA: ZDE BYLO PROTECTED, NYNÍ PUBLIC ***
-        // Díky tomu můžeš číst q[4] a měnit sigma parametry přímo ze sketche.
-  
+public: 
   ICM42688P* _imu;
   AK09918C* _mag;
 
-  // Quaternion (w, x, y, z)
+  // Orientace (Quaternion)
   float q[4] = {1.0f, 0.0f, 0.0f, 0.0f};
 
-  // Calibration data
-  float gyroOffset[3] = {0, 0, 0};
-  float accelOffset[3] = {0, 0, 0};
-  float magHardIron[3] = {0, 0, 0};
-  float magSoftIron[2][3] = {{1, 0, 0}, {0, 1, 0}}; // Zjednodušeno 2D nebo 3x3 dle implementace
+  // --- PUBLIC DATA (RAW) ---
+  float lastAx = 0, lastAy = 0, lastAz = 0; // G
+  float lastGx = 0, lastGy = 0, lastGz = 0; // dps
+  float lastMx = 0, lastMy = 0, lastMz = 0; // uT
 
-  // Default Gaussian parameters (pro adaptivní filtr)
-  float accRef = 1.0f;          // Reference 1G
-  float accSigma = 0.0175f;     // Tolerance akcelerometru (pro start rakety nutno snížit/upravit)
-  float magRef = 50.88f;        // Reference síly mag. pole (uT)
-  float magSigma = 3.5f;        // Tolerance magnetometru
-  float magTiltSigmaDeg = 15.0f;// Tilt sigma
-  float yawKi = 0.005f;         // Integral gain pro yaw
-  float maxAccelGain = 0.5f;    // Omezení maximálního vlivu akcelerometru
-  float maxMagGain = 0.5f;      // Omezení maximálního vlivu magnetometru
+  // --- KALIBRAČNÍ DATA (Public pro zápis z main) ---
+  float accelOffset[3] = {0.0f, 0.0f, 0.0f};      // Offset akcelerometru
+  float magHardIron[3] = {0.0f, 0.0f, 0.0f};      // Hard Iron (bias)
+  float magSoftIron[3][3] = {{1,0,0},{0,1,0},{0,0,1}}; // Soft Iron (matice)
 
-  // Internal state
+  // --- PARAMETRY FILTRU (Public pro ladění) ---
+  float accRef = 1.0f;          
+  float accSigma = 0.05f;       
+  float magRef = 50.0f;         
+  float magSigma = 5.0f;       
+  float magTiltSigmaDeg = 20.0f;
+  float magneticDeclination = 0.0f;
+
+  float maxAccelGain = 0.1f;    
+  float maxMagGain = 0.1f;      
+
   unsigned long lastTime = 0;
-  float gyroBiasZ = 0.0f;
 
-  // Accumulation for acceleration
-  float axSum = 0.0f;
-  float aySum = 0.0f;
-  float azSum = 0.0f;
-  int accSamples = 0;
-
-  // Interval pro kontrolu magnetometru (šetření CPU)
-  unsigned long magReadIntervalUs = 2000; // Čteme mag méně často (např. 500Hz)
-  unsigned long lastMagReadAttempt = 0;
-
-  // Gaussian gain function
+  // Interní metody
   float gaussianGain(float x, float mu, float sigma);
-
-  // Get correction angles from accel and mag (in degrees)
-  void getCorrectionAngles(float ax, float ay, float az, float mx, float my, float mz, float& roll, float& pitch, float& yaw);
-
-  // Gyro integration step
   void gyroIntegration(float gx, float gy, float gz, float dt);
-
-  // Correction step
-  void complementaryCorrection(float ax, float ay, float az, float mx, float my, float mz, float dt);
+  void getCorrectionAngles(float ax, float ay, float az, float mx, float my, float mz, float& roll, float& pitch, float& yaw);
 
 public:
   TriSenseFusion(ICM42688P* imu, AK09918C* mag);
   
-  // Hlavní update funkce
   virtual bool update() = 0;
-
-  // Helpery
-  void initOrientation(int samples = 100);
+  
+  // Funkce pro kalibraci akcelerometru na rampě (předpokládá kolmou raketu)
+  void calibrateAccelStatic(int samples = 1000);
+  
+  void initOrientation(int samples = 200);
   void getOrientationDegrees(float& roll, float& pitch, float& yaw);
   
-  // Setters
+  // Setters (pro pohodlí, ale proměnné jsou public)
   void setAccelGaussian(float ref, float sigma);
   void setMagGaussian(float ref, float sigma, float tiltSigma);
-  void setGains(float maxAcc, float maxMag, float ki);
+  void setMagCalibration(float hardIron[3], float softIron[3][3]);
   void setDeclination(float deg);
-
-  float magneticDeclination = 0.0f;
 };
 
-// Advanced implementation using Adaptive Complementary Filter
+// Advanced implementation
 class AdvancedTriFusion : public TriSenseFusion {
 public:
   AdvancedTriFusion(ICM42688P* imu, AK09918C* mag);
   bool update() override;
-  
-  // Veřejné metody pro nastavení limitů zisku (užitečné pro ladění za letu)
-  void setMaxGains(float accGain, float magGain) {
-      maxAccelGain = accGain;
-      maxMagGain = magGain;
-  }
 };
 
 #endif
